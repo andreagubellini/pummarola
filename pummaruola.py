031d26fc53f1c513404efe2f249a0706988af93c
Andrea Gubellini <andreagubellini@gmail.com>
2021-01-18 22:29:58 +0100

    initial commit

---
+++ pummaruola.py
@@ -0,0 +1,51 @@
+import sys
+import asyncio
+import toml
+import discord
+from discord.ext import commands
+
+def main():
+    pomodoro_standard = 300
+    with open("config.toml", mode="r") as config_file:
+        config = toml.load(config_file)
+        
+    token = config["token"]["token"]
+
+    client = commands.Bot(command_prefix='!')
+
+    @client.event
+    async def on_message(message):
+	    await client.process_commands(message)
+
+    @client.command()
+    async def pummaruola(ctx, *args):
+        for arg in args:
+            print(type(arg))
+            if arg is None:
+                await asyncio.sleep(arg)
+                await pomodoro_end(ctx)
+            elif arg in ["-h", "--help"]:
+                embed = discord.Embed(title="Commands", description="Pummaruola commands list", color=0x00ff00)
+                embed.add_field(name="!pummaruola -h", value="toggles pummaruola commands", inline=False)
+                embed.add_field(name="!pummaruola --help", value="toggles pummaruola commands", inline=False)
+                embed.add_field(name="!pummaruola", value="starts a default 5 minutes pomodoro timers", inline=False)
+                embed.add_field(name="!pummaruola X", value="starts a pomodoro where timer equals X (X is seconds)", inline=False)
+                await ctx.channel.send(embed=embed)
+            elif isinstance(arg, str) not in ["-h", "--help"]:
+                arg = int(arg)
+                for left in range(arg, 0, -1):
+                    sys.stdout.write("\r")
+                    sys.stdout.write("{:2d} seconds remaining.".format(left))
+                    sys.stdout.flush()
+                    await asyncio.sleep(1)
+                await pomodoro_end(ctx)
+            else:
+                break
+
+    async def pomodoro_end(ctx):
+        await ctx.author.send('Your Pummaruola timer has come to an end!')
+
+    client.run(token)
+
+if __name__ == "__main__":
+    main()
